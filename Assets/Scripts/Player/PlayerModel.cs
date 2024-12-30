4a3a27e9ed54e812d922b6c1f078742740539d63
Tomasz Ciechanowski <ciechantako@gmail.com>
2024-12-30 20:03:41 +0100

    player stats setup

---
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModel
+{
+    private PlayerStats _playerData;
+    private int _healthPoints;
+    private float _movementSpeed;
+    private float _armor;
+    private float _stamina;
+}
---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats
+{
+    private int _strength;
+    private int _agility;
+}
