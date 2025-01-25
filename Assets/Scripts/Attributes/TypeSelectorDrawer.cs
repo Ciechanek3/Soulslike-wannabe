fd2a87e1200c47c62fb49e215686cc6b7f96d574
Tomasz Ciechanowski <ciechantako@gmail.com>
2025-01-25 15:04:22 +0100

    cached values so it no longer lags in inspector

--- Assets/Scripts/Attributes/TypeSelectorDrawer.cs
+++ Assets/Scripts/Attributes/TypeSelectorDrawer.cs
@@ -7,8 +7,33 @@ using System.Reflection;
 
 public abstract class TypeSelectorDrawer<T> : PropertyDrawer
 {
-    private readonly Dictionary<string, FieldInfo[]> fieldCache = new Dictionary<string, FieldInfo[]>();
-    private readonly Dictionary<string, object> instanceCache = new Dictionary<string, object>();
+    private static Dictionary<Type, Type[]> classCache = new Dictionary<Type, Type[]>();
+    private static Dictionary<string, FieldInfo[]> fieldCache = new Dictionary<string, FieldInfo[]>();
+    private static Dictionary<string, object> instanceCache = new Dictionary<string, object>();
+
+    private Type[] GetClassesImplementingInterface(Type targetType)
+    {
+        if (!classCache.TryGetValue(targetType, out var types))
+        {
+            types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => targetType.IsAssignableFrom(t) && !t.IsAbstract)
+                .ToArray();
+
+            classCache[targetType] = types;
+        }
+        return types;
+    }
+
+    private object GetOrCreateInstance(string className, Type classType)
+    {
+        if (!instanceCache.TryGetValue(className, out var instance))
+        {
+            instance = Activator.CreateInstance(classType);
+            instanceCache[className] = instance;
+        }
+        return instance;
+    }
 
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
@@ -18,89 +43,61 @@ public abstract class TypeSelectorDrawer<T> : PropertyDrawer
             return;
         }
 
-        // Get the attribute and its target type
         var targetType = typeof(T);
-
-        // Find classes implementing the target type
-        var types = AppDomain.CurrentDomain.GetAssemblies()
-            .SelectMany(a => a.GetTypes())
-            .Where(t => targetType.IsAssignableFrom(t) && !t.IsAbstract)
-            .ToArray();
-
+        var types = GetClassesImplementingInterface(targetType);
         var classNames = types.Select(t => t.Name).ToArray();
 
-        string selectedClass = property.stringValue;
-        int selectedIndex = Mathf.Max(0, Array.IndexOf(classNames, selectedClass));
-        selectedIndex = EditorGUI.Popup(new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight), label.text, selectedIndex, classNames);
+        EditorGUI.BeginProperty(position, label, property);
 
-        selectedClass = classNames[selectedIndex];
-        property.stringValue = selectedClass;
+        EditorGUILayout.LabelField(label.text, EditorStyles.boldLabel);
 
-        if (!instanceCache.TryGetValue(selectedClass, out var instance))
+        // Dropdown for selecting class
+        int selectedIndex = Mathf.Max(0, Array.IndexOf(classNames, property.stringValue));
+        EditorGUI.BeginChangeCheck();
+        selectedIndex = EditorGUILayout.Popup("Select Class", selectedIndex, classNames);
+
+        if (EditorGUI.EndChangeCheck())
         {
-            instance = Activator.CreateInstance(types[selectedIndex]);
-            instanceCache[selectedClass] = instance;
+            property.stringValue = classNames[selectedIndex];
         }
 
+        // Get selected class and instance
+        var selectedClass = classNames[selectedIndex];
+        var selectedClassType = types[selectedIndex];
+        var instance = GetOrCreateInstance(selectedClass, selectedClassType);
+
+        // Get or cache fields
         if (!fieldCache.TryGetValue(selectedClass, out var fields))
         {
-            fields = types[selectedIndex].GetFields().Where(f => f.IsPublic && !f.IsStatic).ToArray();
+            fields = selectedClassType.GetFields().Where(f => f.IsPublic && !f.IsStatic).ToArray();
             fieldCache[selectedClass] = fields;
         }
 
-        float yOffset = EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+        // Display editable fields
         foreach (var field in fields)
         {
-            Rect fieldRect = new Rect(position.x, position.y + yOffset, position.width, EditorGUIUtility.singleLineHeight);
             var fieldValue = field.GetValue(instance);
-
             string labelWithType = $"{field.FieldType.Name} {field.Name}";
 
-            object newValue = new();
-            switch (fieldValue)
-            {
-                case int intValue:
-                    newValue = EditorGUI.IntField(fieldRect, labelWithType, intValue);
-                    break;
-                case float floatValue:
-                    newValue = EditorGUI.FloatField(fieldRect, labelWithType, floatValue);
-                    break;
-                case string stringValue:
-                    newValue = EditorGUI.TextField(fieldRect, labelWithType, stringValue);
-                    break;
-                case bool boolValue:
-                    newValue = EditorGUI.Toggle(fieldRect, labelWithType, boolValue);
-                    break;
-                case Vector3 vectorValue:
-                    newValue = EditorGUI.Vector3Field(fieldRect, labelWithType, vectorValue);
-                    break;
-                default:
-                    EditorGUI.LabelField(fieldRect, labelWithType, $"Add type: {field.FieldType.Name}");
-                    newValue = null;
-                    break;
-            }
-
-            // Update the field value if it changes
-            if (newValue != null && !Equals(newValue, fieldValue))
-            {
-                field.SetValue(instance, newValue);
-            }
-
-            yOffset += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
-        }  
+            if (fieldValue is int intValue)
+                field.SetValue(instance, EditorGUILayout.IntField(labelWithType, intValue));
+            else if (fieldValue is float floatValue)
+                field.SetValue(instance, EditorGUILayout.FloatField(labelWithType, floatValue));
+            else if (fieldValue is string stringValue)
+                field.SetValue(instance, EditorGUILayout.TextField(labelWithType, stringValue));
+            else if (fieldValue is bool boolValue)
+                field.SetValue(instance, EditorGUILayout.Toggle(labelWithType, boolValue));
+            else if (fieldValue is Vector3 vectorValue)
+                field.SetValue(instance, EditorGUILayout.Vector3Field(labelWithType, vectorValue));
+            else
+                EditorGUILayout.LabelField($"{field.Name} (Unsupported Type)");
         }
+
+        EditorGUI.EndProperty();
+    }
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        // Calculate height based on the selected class's fields
-        if (property.propertyType != SerializedPropertyType.String) return EditorGUIUtility.singleLineHeight;
-
-        if (fieldCache.TryGetValue(property.stringValue, out var fields))
-        {
-            return EditorGUIUtility.singleLineHeight * (1 + fields.Length) +
-                   EditorGUIUtility.standardVerticalSpacing * fields.Length;
-        }
-
         return EditorGUIUtility.singleLineHeight;
     }
 }
